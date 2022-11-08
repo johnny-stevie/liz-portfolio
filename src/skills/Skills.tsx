d2256cba2392f7fbe5bbd7ffc634d9511a4afc60
johnny-stevie <johnstevenwall@gmail.com>
2022-11-08 15:58:50 +0000

    - home page done - skills page start - add react/js icons

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Skills.css";
+import { ReactIcon } from "../icons/ReactIcon";
+import { JSIcon } from "../icons/JSIcon";
 export function Skills() {
   return (
     <div className="skills-section">
-      <div className="skills-panel"></div>
+      <div className="skills-panel">
+        <span className="skills-title">My Skills</span>
+      </div>
       <div className="skills-side-panel">
-        <span>extra stuff extra</span>
+        <div className="icons-row">
+          <ReactIcon />
+          <JSIcon />
+        </div>
       </div>
     </div>
   );
