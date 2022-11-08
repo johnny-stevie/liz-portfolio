67ff09add57bbe47567aa444d197be1276d9e1cf
johnny-stevie <johnstevenwall@gmail.com>
2022-11-08 14:45:26 +0000

    Add the rest of the files

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Skills.css";
+export function Skills() {
+  return (
+    <div className="skills-section">
+      <div className="skills-panel"></div>
+      <div className="skills-side-panel">
+        <span>extra stuff extra</span>
+      </div>
+    </div>
+  );
+}
