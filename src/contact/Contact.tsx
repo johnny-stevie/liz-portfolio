4a3eb619f4cc2511436ab015c0a2b8d888be3360
John Wall <johnstevenwall@gmail.com>
2025-02-10 16:54:55 +0000

    new version

--- src/contact/Contact.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Contact.css";
-export function Contact() {
-  return (
-    <div className="contact-panel">
-      <span className="contact-title">Contact me</span>
-    </div>
-  );
-}
